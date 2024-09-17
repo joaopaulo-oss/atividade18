540fb7cd1ebb7245902795ab0154dc82c61b4de5
joaopaulo-oss <joaopaulo24973@gmail.com>
2024-09-17 08:45:25 -0300

    estrutura de repetição

--- atv18.py
+++ atv18.py
@@ -1,4 +1,8 @@
 # Crie um programa que utilize uma estrutura de repetição para imprimir os números de 1 a 10.
+numero = [0,1,2,3,4,5,6,7,8,9,10]
 
-for numero in range(1, 10):
-        print(numero)
+for numero in range(11):
+    print(numero)
+
+else:
+    print('todos os numeros foram repetidos')
